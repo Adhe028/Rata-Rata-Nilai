64ff1c82cfe1af2c61afe1ed1a501e293fe46f45
Adhe kurniawan <46317980+Adhe028@users.noreply.github.com>
2019-11-06 10:33:53 +0700

    selamat mengCopy

---
+++ semester3/src/uts/uts.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts;
+
+/**
+ *
+ * @author ACER
+ */
+public class uts {
+    public static double rata(double binggris, double indonesia , double biologi, double kimia, double fisika, double matematika, double sejarah, double ekonomi, double sosiologi){
+     return (binggris+indonesia+biologi+kimia+fisika+matematika+sejarah+ekonomi+sosiologi)/9;
+    }
+
+    
+}
